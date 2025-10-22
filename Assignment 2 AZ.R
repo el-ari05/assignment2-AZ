0067e4f347053056f20bb0c3b28e71cf08b8bd4a
el-ari05 <arianzarazua@gmail.com>
2025-10-21 20:53:53 -0400

    Update Assignment 2 AZ.R

--- Assignment 2 AZ.R
+++ Assignment 2 AZ.R
@@ -1,1 +1,8 @@
 #This is the code for assignment 2
+
+penguin <- read.csv("C:/Users/arian/OneDrive - wesleyan.edu/4 QAC 380/penguins.csv")
+
+table(penguin$sex)
+summary(penguin$body_mass_g)
+summary(penguin$bill_length_mm)
+
